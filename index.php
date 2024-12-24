debc864bf4bbc8d9c20993b6654f09b4f2aabdb8
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-12-24 09:51:45 +0200

    add HTML

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>To-Do List</title>
+    <link rel="stylesheet" href="css/">
+</head>
+<body>
+<div class="main-section">
+    <div class="add-section">
+        <form action="" >
+            <input type="text" name="title" placeholder="This field is required /">
+            <button type="submit">Add &nbsp;
+                <span>&#43;</span>
+            </button>
+        </form>
+    </div>
+    <div class="show=todo-section">
+        <div class="todo_item">
+            <input type="checkbox">
+            <h2>This is hhsx nknak</h2>
+            <br>
+            <small>Created: 3/3/2020</small>
+        </div>
+    </div>
+</div>
+</body>
+</html>

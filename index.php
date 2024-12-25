2fe1b3e42cb5a19dd09dea63b0393e93f0d2b2cb
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-12-25 10:42:16 +0200

    Edit

--- index.php
+++ index.php
@@ -1,89 +1,91 @@
 <?php
 require 'db_conn.php';
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>To-Do List</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 <div class="main-section">
-    <div class="add-section" >
-        <form action="app/add.php" method = "POST" autocomplete="off">
-        <?php if(isset($_GET['mess']) && isset($_GET['mess']) == 'error'){ ?>
-            <input type="text" name="title" style="border-color: #ff6666" placeholder="This field is required"/>
-            <button type="submit">Add &nbsp;
-                <span>&#43;</span>
-            </button>
-        <?php } else { ?>
-            <input type="text" name="title"  placeholder="What do you need to do ?"/>
-            <button type="submit">Add &nbsp;
-                <span>&#43;</span>
-            </button>
+    <div class="add-section">
+        <form action="app/add.php" method="POST" autocomplete="off">
+            <?php if(isset($_GET['mess']) && $_GET['mess'] == 'error'){ ?>
+                <input type="text" name="title" style="border-color: #ff6666" placeholder="This field is required" />
+                <button type="submit">Add &nbsp; <span>&#43;</span></button>
+
+            <?php }else{ ?>
+                <input type="text" name="title" placeholder="What do you need to do?" />
+                <button type="submit">Add &nbsp; <span>&#43;</span></button>
             <?php } ?>
         </form>
     </div>
     <?php
-    $stmt = $conn->query("SELECT * FROM todos ORDER BY id DESC");
+    $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC");
     ?>
     <div class="show-todo-section">
-        <?php if ($stmt->rowCount() <= 0) { ?>
-            <div class="todo_item">
+        <?php if($todos->rowCount() <= 0){ ?>
+            <div class="todo-item">
                 <div class="empty">
-                    <img src="img/f.png" width="100%">
+                    <img src="img/f.png" width="100%" />
                     <img src="img/Ellipsis.gif" width="80px">
                 </div>
             </div>
-        <?php } else {
-            while ($todo = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
-                <div class="todo_item">
-                    <span id="<?php echo  $todo['id']; ?>" class="remove-to-do">x</span>
-                    <?php if ($todo['checked']){?>
-                        <input type="checkbox" data-todo-id = "<?php echo  $todo['id']; ?>" class="check-box" checked>
-                        <h2 class="checked"><?php echo htmlspecialchars($todo['title']); ?></h2>
-                        <?php } else {?>
-                        <input type="checkbox" data-todo-id = "<?php echo  $todo['id']; ?>" class="check-box" >
-                        <h2><?php echo htmlspecialchars($todo['title']); ?></h2>
-                        <?php } ?>
-                    <small>Created: <?php echo htmlspecialchars($todo['date_time']); ?></small>
-                </div>
-            <?php }
-        } ?>
+        <?php } ?>
+
+        <?php while($todo = $todos->fetch(PDO::FETCH_ASSOC)) { ?>
+            <div class="todo-item">
+                    <span id="<?php echo $todo['id']; ?>"
+                          class="remove-to-do">x</span>
+                <?php if($todo['checked']){ ?>
+                    <input type="checkbox" class="check-box" data-todo-id ="<?php echo $todo['id']; ?>" checked />
+                    <h2 class="checked"><?php echo $todo['title'] ?></h2>
+                <?php }else { ?>
+                    <input type="checkbox" data-todo-id ="<?php echo $todo['id']; ?>" class="check-box" />
+                    <h2><?php echo $todo['title'] ?></h2>
+                <?php } ?>
+                <br>
+                <small>created: <?php echo $todo['date_time'] ?></small>
+            </div>
+        <?php } ?>
     </div>
 </div>
+
 <script src="js/jquery-3.2.1.min.js"></script>
+
 <script>
     $(document).ready(function(){
-        alert("hello");
-    });
-    $('.remove-to-do').click(function(){
-        const id = $(this).attr('id');
-        $.post("app/remove.php",{
-            id: id
-            },
-            (data) => {
-            if(data){
-                $(this).parent().hide(600);
-            }
-        });
-        $(".check-box").click(function(e){
-            const id = $(this).attr('data-todo-id');
-            $.post('app/check.php', {id: id},
-                (data) => {
-                if(data != 'error'){
-                    const h2 = $(this).next();
-                    if(data === '1'){
-                        h2.removeClass('checked');
-                    }else {
-                        h2.addClass('checked');
+        $('.remove-to-do').click(function(){
+            const id = $(this).attr('id');
+            $.post("app/remove.php", {id: id},
+                (data)  => {
+                    if(data){
+                        $(this).parent().hide(600);
                     }
                 }
+            );
+        });
+
+        $(".check-box").click(function(e){
+            const id = $(this).attr('data-todo-id');
+
+            $.post('app/check.php',
+                {id: id},
+                (data) => {
+                    if(data != 'error'){
+                        const h2 = $(this).next();
+                        if(data === '1'){
+                            h2.removeClass('checked');
+                        }else {
+                            h2.addClass('checked');
+                        }
+                    }
                 }
-            )
+            );
         });
     });
 </script>
